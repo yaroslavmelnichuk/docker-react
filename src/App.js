82327b5e1fe9d38897cf9639c5e2b9e67969bbcc
Yaroslav Melnichuk <yariksuperman95@gmail.com>
2021-06-04 10:23:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React ttt
           </a>
         </header>
       </div>
